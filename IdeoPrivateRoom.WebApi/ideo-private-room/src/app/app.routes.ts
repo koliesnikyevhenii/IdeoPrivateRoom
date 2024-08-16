979cefb37f226d16cd250c3c50bc463cdb341448
Herman Orlov <herman.orlov@ideo-digital.com>
2024-08-16 11:59:51 +0300

    Added global header with routes

--- IdeoPrivateRoom.WebApi/ideo-private-room/src/app/app.routes.ts
+++ IdeoPrivateRoom.WebApi/ideo-private-room/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+// import { eventListRoutes } from './event-list/event-list.routes';
+import { CalendarComponent } from './calendar/calendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // ...eventListRoutes,
+    {
+        path: '',
+        redirectTo: 'calendar',
+        pathMatch: 'full'
+    },
+    {
+        path: 'calendar',
+        component: CalendarComponent,
+        title: 'Calendar'
+    }
+];
